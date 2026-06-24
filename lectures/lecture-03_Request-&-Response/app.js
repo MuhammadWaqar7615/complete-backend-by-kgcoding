559b473b1b02f3cc341b414f1260a513f5ac082b
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-06-24 09:39:29 +0500

    Refactor HTTP server response handling and improve 404 error page

--- lectures/lecture-03_Request-&-Response/app.js
+++ lectures/lecture-03_Request-&-Response/app.js
@@ -1,20 +1,43 @@
 const http = require('http');
 const PORT = 3001;
 const server = http.createServer((req, res) => {
-    console.log("server created");
+    if (req.url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+        <html>
+        <head>
+        <title>Backend Practice</title>
+        </head>
+        
+        <body>
+        <h1>This is Home page</h1>
+        </body>
+        </html>
+        `)
+        return res.end();
+    } else if (req.url === '/products') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+            <html>    
+            <head><title>Backend Practice</title></head>    
+            <body><h1>This is products page</h1></body>
+            </html>    
+        `)
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write(`
         <html>
         <head>
-        <title>This is server page</title>
+        <title>Backend Pracice</title>
         </head>
         
         <body>
-        <h1>This is sent by server</h1>
+        <h1>Error 404 page not found</h1>
         </body>
         </html>
         `)
-    res.end()
+    return res.end();
 })
 server.listen(PORT, () => {
     console.log('server started listening at port: ', PORT);
