c02c7477c30e868e34a4d60102236baa39ffb9be
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-06-25 00:53:36 +0500

    parsing request basics are cleared

---
+++ lectures/chapter-05_Parsing-Request/user.js
@@ -0,0 +1,65 @@
+const http = require("http");
+const fs = require("fs/promises");
+const PORT = 3001;
+const server = http.createServer((req, res) => {
+  const body = [];
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(`
+        <html>
+        <head><title>Form writing</title></head>
+        <body>
+            <h1>Home Page</h1>
+            <form action="/form-detail" method="POST">
+                <label for="username">User name:</label>
+                <input type="text" id="username" name="username"><br>
+                <span>Gender:</span>
+                <label for="male">Male:</label>
+                <input type="radio" id="male" name="gender" value="male" />
+                <label for="female">Female:</label>
+                <input type="radio" id="female" name="gender" value="female" /><br>
+                <button type="Submit">Submit</button>
+            </form>
+        </body>
+        </html>
+    `);
+    return res.end();
+  } else if (req.url.toLowerCase() === "/form-detail" && req.method == "POST") {
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const buffer = Buffer.concat(body);
+      const data = buffer.toString();
+      console.log("data: ", data);
+      const params = new URLSearchParams(data);
+      // const paramsData = {}
+      // for(const [key, val] of params.entries()) {
+      //   paramsData[key] = val;
+      // }
+      const paramsData = Object.fromEntries(params);
+      console.log("params data: ", paramsData);
+      async function writeFile() {
+        await fs.writeFile('user-detail.txt', JSON.stringify(paramsData));
+      }
+      writeFile();
+      res.statusCode = 302;
+      res.setHeader("location", "/");
+      res.end();
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write(`
+        <html>
+        <head><title>Form writing</title></head>
+        <body>
+            <h1>Error 404: Page not found</h1>
+        </body>
+        </html>
+    `);
+    return res.end();
+  }
+});
+server.listen(PORT, () => {
+  console.log("server is listeningi at port: ", PORT);
+});
