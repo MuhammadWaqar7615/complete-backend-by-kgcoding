5aa0082b55895122298e04a64a45424744d48203
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-07-05 23:55:12 +0500

    feat: initialize Express server with routing and middleware setup

---
+++ lectures/chapter-12_Practice/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const router = require("./routes/route");
+const users = require("./data/users");
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  next();
+});
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
