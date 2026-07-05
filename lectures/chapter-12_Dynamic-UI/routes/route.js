77bd7bad99cf81dde3a67deb9e02b1b77504dcd2
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-07-05 23:04:33 +0500

    feat: update route handling to include bodyUrl in home and formDetail views, and add gender to user data

--- lectures/chapter-12_Dynamic-UI/routes/route.js
+++ lectures/chapter-12_Dynamic-UI/routes/route.js
@@ -5,17 +5,17 @@ const path = require('path');
 const users = require('../data/users');
 
 router.get('/', (req, res) => {
-    res.render('home', {title: "practice"});
+    res.render('home', {title: "practice", bodyUrl: req.url});
 })
 
 router.post('/form-detail', (req, res) => {
-    users.push( { username: req.body.username, email: req.body.email});
+    users.push( { username: req.body.username, gender: req.body.gender, email: req.body.email});
     console.log("users :", users);
     res.redirect('/form-detail');
 })
 
 router.get('/form-detail', (req, res) => {
-    res.render('formDetail', { users, title: 'form detail' });
+    res.render('formDetail', { users, title: 'form detail', bodyUrl: req.url });
 });
 
 router.use((req, res) => {
