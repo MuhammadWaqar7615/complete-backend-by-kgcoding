fb205392bb7e1cdd13caf029e30298cdc391e995
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-07-07 23:39:10 +0500

    feat: implement Users class with constructor and save method for user management

--- lectures/chapter-13_MVC/models/users.js
+++ lectures/chapter-13_MVC/models/users.js
@@ -1,2 +1,13 @@
 const users = [];
 module.exports = users;
+
+module.exports = class Users {
+    constructor(username, gender, email) {
+        this.username = username;
+        this.email = email;
+        this.gender = gender;
+    }
+    save() {
+        users.push(this);
+    }
+}
