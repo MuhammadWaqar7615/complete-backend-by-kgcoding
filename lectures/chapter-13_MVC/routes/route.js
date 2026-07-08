6a99aa22f37ff98ca5f31a8270281b0a5d5a40a1
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-07-08 23:40:35 +0500

    feat: update root route to use getUsers controller

--- lectures/chapter-13_MVC/routes/route.js
+++ lectures/chapter-13_MVC/routes/route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const rootDir = require('../utils/routesUtil');
 const path = require('path');
-const { getHome, postFormDetail, getFormDetail, errorController } = require('../controller/controllers');
+const { getUsers, postFormDetail, getFormDetail, errorController, getUsersler } = require('../controller/controllers');
 
-router.get('/', getHome);
+router.get('/', getUsers);
 router.post('/form-detail', postFormDetail);
 router.get('/form-detail', getFormDetail);
 router.use(errorController)
