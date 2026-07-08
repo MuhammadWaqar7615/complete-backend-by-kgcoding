81b65d0939b8e4a06caada5ba4fd79ce4c5996e6
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-07-08 23:40:05 +0500

    refactor: restructure Users class and enhance user data handling

--- lectures/chapter-13_MVC/models/users.js
+++ lectures/chapter-13_MVC/models/users.js
@@ -1,13 +1,30 @@
-const users = [];
+const path = require("path");
+const rootDir = require("../utils/routesUtil");
+const fs = require("fs");
+
+let users = [];
 module.exports = users;
 
-module.exports = class Users {
-    constructor(username, gender, email) {
-        this.username = username;
-        this.email = email;
-        this.gender = gender;
-    }
-    save() {
-        users.push(this);
-    }
-}
+module.exports = class UsersData {
+  constructor(username, gender, email) {
+    this.username = username;
+    this.email = email;
+    this.gender = gender;
+  }
+  save() {
+    users.push(this);
+    const usersPath = path.join(rootDir, "data", "users.json");
+    fs.writeFile(usersPath, JSON.stringify(users), (err) => {
+      console.log("file wrting errors: ", err);
+    });
+  }
+  static async fetchAll() {
+    // console.log("fetch all function triggered");
+    const usersPath = path.join(rootDir, "data", "users.json");
+    const data = await fs.promises.readFile(usersPath, (err) => {
+        console.log("error while reading file: ", err);
+    });
+    users = JSON.parse(data);
+    return users;
+  }
+};
